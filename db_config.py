4fa76b9aa6b2909e7d85f5d2a7042cfa3052bf33
lya70 <144755738+Oneday1999@users.noreply.github.com>
2024-06-11 13:47:39 +0800

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,6 +2,6 @@
 def get_db_config(): # 获取数据库配置
     host = "localhost"
     user = "root"
-    password = "664210Yly"
+    password = "password"
     database = "task"
     return host, user, password, database
